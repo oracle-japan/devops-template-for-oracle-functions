3ca0ab26fd76c7c2e17a1206b1ba6ca160a4b35d
shukawam <shukawam@gmail.com>
2022-03-14 08:26:11 +0000

    Initial commit

---
+++ fn-hello/src/main/java/com/example/fn/HelloFunction.java
@@ -0,0 +1,10 @@
+package com.example.fn;
+
+public class HelloFunction {
+
+    public String handleRequest() {
+        System.out.println("Inside Java Hello World function"); 
+        return "Hello from Function: 1.0";
+    }
+
+}
